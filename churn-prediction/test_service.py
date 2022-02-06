7c6a4ee49b7059cbd50689b9b66620e9d3fdaf38
mluqmanhakim <mhd.luqmanhakim@gmail.com>
2022-02-06 18:42:58 +0100

    Deploy to aws elastic beanstalk

---
+++ churn-prediction/test_service.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+customer = {
+    'customerid': '8879-zkjof',
+    'gender': 'female',
+    'seniorcitizen': 0,
+    'partner': 'no',
+    'dependents': 'no',
+    'tenure': 10,
+    'phoneservice': 'yes',
+    'multiplelines': 'no',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'yes',
+    'onlinebackup': 'no',
+    'deviceprotection': 'yes',
+    'techsupport': 'yes',
+    'streamingtv': 'yes',
+    'streamingmovies': 'yes',
+    'contract': 'one_year',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'bank_transfer_(automatic)',
+    'monthlycharges': 79.85,
+    'totalcharges': 3320.75
+}
+
+host = 'churn-serving-env-v2.eba-umqewhdk.eu-west-1.elasticbeanstalk.com'
+url = f'http://{host}/predict'
+response = requests.post(url, json=customer)
+result = response.json()
+
+print(json.dumps(result, indent=2))
